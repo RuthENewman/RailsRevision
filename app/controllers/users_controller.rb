c3525b653c8f454777349f37b50d32fb56652c3b
Ruth Newman <ruthnewman@gmail.com>
2020-02-27 22:14:59 +0000

    adding first passing tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :destroy, :edit, :update]
+
+    def index 
+        @users = User.all 
+    end
+
+    def show
+    end 
+
+    def new 
+        @user = User.new
+    end 
+
+    def destroy
+        if @user
+            @user.destroy
+        else 
+            
+        end
+    end
+
+    private
+
+    def set_user
+        @user = User.find(params[:id])
+    end
+
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    context 'validation tests' do 
+      it 'ensures first name presence' do
+        user = User.new(last_name: 'Newman', email: 'testemail@email.com').save
+        expect(user).to eq(false)
+      end 
+
+      it 'ensures last name presence' do
+        user = User.new(first_name: 'Ruth', email: 'testemail@email.com').save
+        expect(user).to eq(false)
+      end
+
+      it 'ensures email presence' do
+        user = User.new(first_name: 'Ruth', last_name: 'Newman').save
+        expect(user).to eq(false)
+      end 
+
+      it 'should save correctly' do 
+        user = User.new(first_name: 'Ruth', last_name: 'Newman', email: 'testemail@email.com').save
+        expect(user).to eq(true)
+      end
+
+    end
+    context 'scope tests' do 
+
+    end
 end
